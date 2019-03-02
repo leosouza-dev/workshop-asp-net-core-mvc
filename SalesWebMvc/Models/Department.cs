5594a6f2be5e8004526541081dcfad7a83873c4c
Leonardo <leonardo@sarival.com.br>
2019-03-01 22:34:00 -0300

    First Model_Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        //propriedades da clase
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
