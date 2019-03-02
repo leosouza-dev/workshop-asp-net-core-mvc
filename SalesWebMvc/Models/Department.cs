34c44934e1a85d0e2fcb0a986c9e2fb2275a2ca2
Leonardo <leonardo@sarival.com.br>
2019-03-02 00:46:34 -0300

    othres entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,5 +1,8 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -11,5 +14,37 @@ namespace SalesWebMvc.Models
 
         [Required(ErrorMessage = "Campo Obrigátório")]
         public string Name { get; set; }
+
+        //associações - departamento possui varios vendedores
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+
+        //CONSTRUTORES
+        public Department()
+        {
+                
+        }
+
+        //obs: não incluir argumentos que são coleções
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //METODOS
+
+        //Add vendedor
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        //reultado de total de vendas do departamento em periodo
+        //vamos pegar a lista de vendedores e somar o total de venda de cada vendedor nesse periodo
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public double BaseSalary { get; set; }
+
+        //associação - um vendedor possui um departamento
+        public Department Department { get; set; }
+        //associação - um vendedor possui muitas salesRecord
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        //CONSTRUTORES
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        //METODOS CUSTOMIZADOS
+
+        //vamos adicionar uma venda em nossa lista de vendas
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        //remove a venda
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        //total de vendas em um determinado periodo
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //usar o Linq para filtrar num determinado periodo
+            //Sales é a lista de vendas
+            //após isso é filtrado
+            //e por fim, fazemos a soma do amount
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
