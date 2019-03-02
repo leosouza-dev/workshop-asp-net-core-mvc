89215a365669c400186e07a5f310b149918ac7e2
Leonardo <leonardo@sarival.com.br>
2019-03-01 23:15:39 -0300

    CRUD Scaffolding

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,10 +1,15 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         //propriedades da clase
+        [Required]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigátório")]
         public string Name { get; set; }
     }
 }
